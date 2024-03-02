254cfaa218079426f1489904ae33390ba53c5fba
ardhrubo <ardhrubo3038@gmail.com>
2024-03-02 12:25:36 +0600

    added

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ app.use(express.static('api'));
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
